f4eef5f55fa99e63d25670879ae04ab93bd4a570
twisterwynny <wynny.eu@gmail.com>
2020-02-18 23:09:49 -0300

    Update acesso.php
    em todos os inputs agora tem atributo required=""

--- acesso.php
+++ acesso.php
@@ -14,11 +14,11 @@
         <form class="form-signin" method="POST" action="access/checar.php">
           <div class="form-group">    
             <label for="email"> Usuário: </label>
-            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Digite o E-mail" required autofocus>              
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Digite o E-mail" required="">              
           </div>
           <div class="form-group">
             <label for="senha"> Senha: </label>
-            <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite a Senha">
+            <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite a Senha" required="">
           </div>
           <div class="text-center">
             <button class="btn btn-secondary" type="button"  data-dismiss="modal"> Cancelar </button>        
@@ -41,56 +41,56 @@
         <form method="POST" action="escola/proc-cad-escola.php">
           <div class="form-group">    
             <label for="nome"> Nome: </label>
-            <input type="text" class="form-control" name="nome" id="nome" aria-describedby="emailHelp" placeholder="Digite o nome da Instituição" required autofocus>              
+            <input type="text" class="form-control" name="nome" id="nome" aria-describedby="emailHelp" placeholder="Digite o nome da Instituição" required="">              
           </div>          
           <div class="form-group">    
             <label for="email"> E-mail: </label>
-            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Digite o E-mail">              
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Digite o E-mail" required="">              
           </div>
           <div class="form-group">
             <label for="senha"> Senha: </label>
-            <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite a Senha">
+            <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite a Senha" required="">
           </div>
           <div class="form-group">
             <label for="fone"> Telefone: </label>
-            <input type="text" class="form-control" name="fone" id="fone" placeholder="Digite o Telefone">
+            <input type="text" class="form-control" name="fone" id="fone" placeholder="Digite o Telefone" required="">
           </div>
           <h6 align="center">E N D E R E Ç O : </h6>
           <div class="form-group">
             <label for="cep"> CEP: </label>
-            <input type="text" class="form-control" name="cep" id="cep" placeholder="Digite o CEP">
+            <input type="text" class="form-control" name="cep" id="cep" placeholder="Digite o CEP" required="">
           </div>
           <div class="form-group">
             <label for="rua"> Rua/Avenida/Caminho: </label>
-            <input type="text" class="form-control" name="rua" id="rua" placeholder="Digite a Rua/Avenida/Caminho">
+            <input type="text" class="form-control" name="rua" id="rua" placeholder="Digite a Rua/Avenida/Caminho" required="">
           </div>
           <div class="form-group">
             <label for="numero"> Número: </label>
-            <input type="text" class="form-control" name="numero" id="numero" placeholder="Digite o Número da casa / prédio">
+            <input type="text" class="form-control" name="numero" id="numero" placeholder="Digite o Número da casa / prédio" required="">
           </div>
           <div class="form-group">
             <label for="bairro"> Bairro/Conjunto: </label>
-            <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Digite o Bairro/Conjunto">
+            <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Digite o Bairro/Conjunto" required="">
           </div>
           <div class="form-group">
             <label for="complemento"> Complemento: </label>
-            <input type="text" class="form-control" name="complemento" id="complemento" placeholder="Digite o Complemento">
+            <input type="text" class="form-control" name="complemento" id="complemento" placeholder="Digite o Complemento" required="">
           </div>
           <div class="form-group">
             <label for="ponto_referencia"> Ponto de Referência: </label>
-            <input type="text" class="form-control" name="ponto_referencia" id="ponto_referencia" placeholder="Digite o Ponto de Referência">
+            <input type="text" class="form-control" name="ponto_referencia" id="ponto_referencia" placeholder="Digite o Ponto de Referência" required="">
           </div>
           <div class="form-group">
             <label for="cidade"> Cidade: </label>
-            <input type="text" class="form-control" name="cidade" id="cidade" placeholder="Digite a Cidade">
+            <input type="text" class="form-control" name="cidade" id="cidade" placeholder="Digite a Cidade" required="">
           </div>
           <div class="form-group">
             <label for="estado"> Estado / UF: </label>
-            <input type="text" class="form-control" name="estado" id="estado" placeholder="Digite o Estado / UF">
+            <input type="text" class="form-control" name="estado" id="estado" placeholder="Digite o Estado / UF" required="">
           </div>
           <div class="form-group">
             <label for="pais"> País: </label>
-            <input type="text" class="form-control" name="pais" id="pais" placeholder="Digite o País">
+            <input type="text" class="form-control" name="pais" id="pais" placeholder="Digite o País" required="">
           </div>
           <div class="text-center">
             <button class="btn btn-secondary" type="button"  data-dismiss="modal"> Cancelar </button>        
