9d85f903cf7f3a93eecf87b7e3c7a69019d6f0f1
twisterwynny <wynny.eu@gmail.com>
2020-02-29 16:22:27 -0300

    Update ALL3.sql
    mudanças no BD.

--- SQLqueries/ALL3.sql
+++ SQLqueries/ALL3.sql
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     ponto_referencia VARCHAR(100) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
     estado VARCHAR(25) NOT NULL,
-    pais VARCHAR(25) NOT NULL    
+    pais VARCHAR(25) NOT NULL,
+    excluido INT
 	-- fim_semestre datetime -- PODE SER NULO pois nem todo usuário tem uma data de fim de semestre.
 )	ENGINE = innodb;
 
@@ -58,6 +59,7 @@ CREATE TABLE IF NOT EXISTS eventos (
     title VARCHAR(100), -- VARIÁVEL DO FULL CALENDAR NÃO PODE MUDAR O NOME
     descricao VARCHAR(500),
     vagas INT,
+    vagas_abertas INT NOT NULL, -- inicia com valor 0 (zero) e, caso alguma escola não confirme, as vagas dela vão para essa coluna
     start datetime NOT NULL, -- VARIÁVEL DO FULL CALENDAR NÃO PODE MUDAR O NOME
     end datetime NOT NULL, -- VARIÁVEL DO FULL CALENDAR NÃO PODE MUDAR O NOME
     color VARCHAR(10), -- VARIÁVEL DO FULL CALENDAR NÃO PODE MUDAR O NOME
@@ -68,26 +70,56 @@ CREATE TABLE IF NOT EXISTS agendamentos (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     evento INT NOT NULL,
     escola INT NOT NULL,
-    turma INT NOT NULL,    
+    turma INT NOT NULL,
+    avisado INT,
+    confirmado INT NOT NULL,
+    excluido INT,
     FOREIGN KEY (evento) REFERENCES eventos (id),
     FOREIGN KEY (escola) REFERENCES usuarios (id),
     FOREIGN KEY (turma) REFERENCES turmas (id)        
 )	ENGINE = innodb;
 
+CREATE TABLE IF NOT EXISTS lista_espera (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    evento INT NOT NULL,
+    escola INT NOT NULL,
+    turma INT NOT NULL,    
+    qtd_alunos_turma INT NOT NULL, -- se QTD_VAGAS == 0, então o pedido é para entrar na lista de espera, caso contrário, então o pedido é de abertura de mais vagas    
+    avisado INT NOT NULL,
+    confirmado INT NOT NULL,
+    excluido INT,
+    -- data_hora DATETIME NOT NULL,
+    FOREIGN KEY (evento) REFERENCES eventos (id),
+    FOREIGN KEY (escola) REFERENCES usuarios (id),
+    FOREIGN KEY (turma) REFERENCES turmas (id)    -- ADICIONAR NOVA COLUNA data-hora DATETIME NOT NULL para determinar quem fez o pedido primeiro    
+)	ENGINE = innodb;
+
+CREATE TABLE IF NOT EXISTS mais_vagas (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    evento INT NOT NULL,
+    escola INT NOT NULL,
+    turma INT NOT NULL,    
+    qtd_alunos_turma INT NOT NULL, -- se QTD_VAGAS == 0, então o pedido é para entrar na lista de espera, caso contrário, então o pedido é de abertura de mais vagas
+    excluido INT,
+    -- data_hora DATETIME NOT NULL,
+    FOREIGN KEY (evento) REFERENCES eventos (id),
+    FOREIGN KEY (escola) REFERENCES usuarios (id),
+    FOREIGN KEY (turma) REFERENCES turmas (id)    -- ADICIONAR NOVA COLUNA data-hora DATETIME NOT NULL para determinar quem fez o pedido primeiro    
+)	ENGINE = innodb;
+
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Eu", "eu@eu.eu", "123", 1, "09358521", "44085-000", "nasceu aqui", 9, "sei lá", "não tem", "se vira", "FSA", "BA", "BRASIL");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Tu", "tu@tu.tu", "123", 2, "88729518", "77558-880", "KDJF K", 1, "ÁI DON NOU", "DON RAVI", "NOS TRINTA", "WHATS", "GO", "ARGENTINA");
-INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Vc", "vc@vc.vc", "123", 3, "09358521", "00177-066", "GRINGO", 8, "!!!!!!!", "MONEY", "$$$$$", "VEGAS", "CALIFORNICATION", "USA");
+INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Vc", "geisekatty@hotmail.com", "123", 3, "09358521", "00177-066", "GRINGO", 8, "!!!!!!!", "MONEY", "$$$$$", "VEGAS", "CALIFORNICATION", "USA");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Adm", "a@a.a", "123", 4, "0987654321", "88750-321", "SAKJ", 3, "AAAAAAAAAA", "DINHEIRO", "@@@@@", "LONDRINA", "PARANÁ", "NOVA DELI");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Estagiario Um", "i1@i.i", "123", 1, "09358521", "44085-000", "nasceu aqui", 9, "sei lá", "não tem", "se vira", "FSA", "BA", "BRASIL");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Estagiario Dois", "i2@i.i", "123", 1, "88729518", "77558-880", "KDJF K", 1, "ÁI DON NOU", "DON RAVI", "NOS TRINTA", "WHATS", "GO", "ARGENTINA");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Funcionario Um", "stf1@stf.stf", "123", 2, "09358521", "00177-066", "GRINGO", 8, "!!!!!!!", "MONEY", "$$$$$", "VEGAS", "CALIFORNICATION", "USA");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Funcionario Dois", "stf2@stf.stf", "123", 2, "0987654321", "88750-321", "SAKJ", 3, "AAAAAAAAAA", "DINHEIRO", "@@@@@", "LONDRINA", "PARANÁ", "NOVA DELI");
-INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Escola Um", "schl1@schl.schl", "123", 3, "09358521", "44085-000", "nasceu aqui", 9, "sei lá", "não tem", "se vira", "FSA", "BA", "BRASIL");
-INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Escola Dois", "schl2@schl.schl", "123", 3, "88729518", "77558-880", "KDJF K", 1, "ÁI DON NOU", "DON RAVI", "NOS TRINTA", "WHATS", "GO", "ARGENTINA");
+INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Escola Um", "viniciusvieira.eu@gmail.com", "123", 3, "09358521", "44085-000", "nasceu aqui", 9, "sei lá", "não tem", "se vira", "FSA", "BA", "BRASIL");
+INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Escola Dois", "wynny.eu@gmail.com", "123", 3, "88729518", "77558-880", "KDJF K", 1, "ÁI DON NOU", "DON RAVI", "NOS TRINTA", "WHATS", "GO", "ARGENTINA");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Adm UM", "a1@a.a", "123", 4, "09358521", "00177-066", "GRINGO", 8, "!!!!!!!", "MONEY", "$$$$$", "VEGAS", "CALIFORNICATION", "USA");
 INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("Adm DOIS", "a2@a.a", "123", 4, "0987654321", "88750-321", "SAKJ", 3, "AAAAAAAAAA", "DINHEIRO", "@@@@@", "LONDRINA", "PARANÁ", "NOVA DELI");
-
--- INSERT INTO usuarios (nome, email, senha, permissao) VALUES ("Eu1", "eu1@eu.eu", "123", 1);
+INSERT INTO usuarios (nome, email, senha, permissao, fone, cep, rua, numero, bairro, complemento, ponto_referencia, cidade, estado, pais) VALUES ("TATAS SCHOOL", "geisekatty@hotmail.com", "123", 3, "88729518", "77558-880", "KDJF K", 1, "ÁI DON NOU", "DON RAVI", "NOS TRINTA", "WHATS", "GO", "ARGENTINA");
 
 USE antares;
 SELECT * FROM alunos;
@@ -96,11 +128,4 @@ SELECT * FROM prof_resp;
 SELECT * FROM usuarios;
 SELECT * FROM eventos;
 SELECT * FROM agendamentos;
-
--- SELECT start FROM eventos WHERE (estagiario = 1 AND title = 'Fim do Semestre');
--- SELECT COUNT(id) AS ja_fez FROM agendamentos WHERE (evento=13 AND escola=3 AND turma=4);
--- SELECT COUNT(id) AS ja_fez FROM eventos WHERE (estagiario=1 AND start="2020-02-01 00:00:00" AND end="2020-02-02 00:00:00");
--- SELECT COUNT(id) AS ja_fez FROM eventos WHERE (estagiario=1 AND start="2020-01-19 00:00:00" AND end="2020-01-20 00:00:00");
--- SELECT YEAR(NOW());
--- SELECT YEAR(curdate());
--- SELECT curdate();
+SELECT * FROM lista_espera;
