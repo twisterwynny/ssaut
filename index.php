327049d149d5b376b622e78ab48a8f947c7d75b8
twisterwynny <wynny.eu@gmail.com>
2020-03-06 23:26:16 -0300

    CDN local
    arquivos que estavam sendo buscados ON line toda vez que a pagina era carregada e deixavam de funcionar quando estava sem internet. agora todos eles estão reunidos na pasta CDN

--- index.php
+++ index.php
@@ -14,9 +14,11 @@ if (isset($_SESSION['funcionario_agendou']))
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- Bootstrap CSS 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">  -->
+    <link href='CDN/bootstrap.min.css' rel='stylesheet'>
+    <link href='CDN/all.css' rel='stylesheet'>
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="sortcut icon" href="img/antares.png" type=".png">
@@ -25,8 +27,11 @@ if (isset($_SESSION['funcionario_agendou']))
     <link href="./css/login-index.css" rel="stylesheet">
     <link href='./calendario/core/main.css' rel='stylesheet'>
     <link href='./calendario/daygrid/main.css' rel='stylesheet'>
+    <!--
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Jomolhari&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Jomolhari&display=swap" rel="stylesheet">   -->
+    <link href='login-index/roboto.css' rel='stylesheet'>
+    <link href='login-index/jomolhari.css' rel='stylesheet'>
 
     <title> Observatório Antares </title>
 
@@ -149,8 +154,11 @@ if (isset($_SESSION['funcionario_agendou']))
         </div>
     </div>
 </footer>
-
+<!--
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+-->
+<script src="CDN/popper.min.js"></script>
+<script src="CDN/bootstrap.min.js"></script>
 
 </html>
