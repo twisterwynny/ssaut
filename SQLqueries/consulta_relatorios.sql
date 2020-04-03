98f36930f66a2e07ac5079a04674ffb984ff67ee
Juniorlpes <bento.lopes50@gmail.com>
2020-04-03 17:55:10 -0300

    agendamentos cadastrados

--- SQLqueries/consulta_relatorios.sql
+++ SQLqueries/consulta_relatorios.sql
@@ -9,3 +9,5 @@ SELECT nome FROM alunos WHERE alunos.turma = (SELECT turma FROM agendamentos WHE
 SELECT c.nome, eventos.start, eventos.end FROM eventos INNER JOIN (SELECT id,nome FROM usuarios WHERE usuarios.permissao = 1) c WHERE eventos.estagiario = c.id ORDER BY c.nome;
 
 --Agendamentos castrado de visitantes
+--troca o id 1 pelo agendamento escolhido
+SELECT usuarios.nome, turmas.nome_turma, eventos.start FROM usuarios INNER JOIN turmas INNER JOIN eventos INNER JOIN (SELECT escola, turma, evento FROM agendamentos WHERE id = 1) c WHERE usuarios.id = c.escola AND turmas.id = c.turma AND  eventos.id = c.evento;
