2bd822ec341d1c97dff01c7a69f4a146e38e685a
Juniorlpes <bento.lopes50@gmail.com>
2020-04-06 17:07:44 -0300

    atualizando script consultas

--- db/consultasRelatorios.php
+++ db/consultasRelatorios.php
@@ -1,6 +1,7 @@
 <?php  
 	include("conexao.php");
 
+	//Para Estagiário saber os visitantes de um agendamento
 	function getNomeVisitantes($id_agendamento) : array{
 		$sql = "select nome FROM alunos WHERE alunos.turma = (SELECT turma FROM agendamentos WHERE agendamentos.id = ". $id_agendamento ;
 		$result = mysqli_query($sql);
@@ -9,6 +10,7 @@
 		return $row;
 	}
 
+	//Para funcionário saber os horários dos estagiários
 	function getHorarioEstagiario() : array{
 		$sql 	= "select c.nome, eventos.start, eventos.end FROM eventos INNER JOIN (SELECT id,nome FROM usuarios WHERE usuarios.permissao = 1) c WHERE eventos.estagiario = c.id ORDER BY c.nome";
 		$result = mysqli_query($sql);
@@ -17,6 +19,7 @@
 		return $row;
 	}
 
+	//Para funcionário saber os visitantes por agendamento
 	function getVisitantes($id_agendamento) : array{
 		$sql 	= "select usuarios.nome, turmas.nome_turma, eventos.start FROM usuarios INNER JOIN turmas INNER JOIN eventos INNER JOIN (SELECT escola, turma, evento FROM agendamentos WHERE id = " . $id_agendamento . ") c WHERE usuarios.id = c.escola AND turmas.id = c.turma AND  eventos.id = c.evento;"
 		$result = mysqli_query($sql);
