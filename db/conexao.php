24221c88b6c19c35430466b599fa3b3c3467c9b0
twisterwynny <wynny.eu@gmail.com>
2020-04-15 23:23:12 -0300

    Update conexao.php
    VERSÃO FINAL

--- db/conexao.php
+++ db/conexao.php
@@ -1,19 +1,17 @@
 <?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "root";
-	$dbname = "antares";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-	
-	if(!$conn)
-	{
-		die("Falha na conexao: " . mysqli_connect_error());
-	}
-	else
-	{
-		//echo "Conexao realizada com sucesso";
-	}	
-	
+header("Content-type: text/html; charset=utf-8"); // for any php files sets the default character set to be used.
+$servidor = "localhost";
+$usuario = "root";
+$senha = "root";
+$dbname = "antares";
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname); //create a connection
+mysqli_set_charset($conn,"utf8"); // Sets the default character set to be used when sending data from and to the database server.
+if(!$conn)
+{
+	die("Falha na conexao: " . mysqli_connect_error());
+}
+else
+{
+	//echo "Conexao realizada com sucesso";
+}		
 ?>
